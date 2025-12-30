31ea7911e75263f0a7f17b7d1e061a9d2f43bf7e
SmitVaghasiya24 <smitvaghasiya89@gmail.com>
2025-12-30 21:40:39 +0530

    update code for today

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,16 @@
-import mysql from 'mysql2/promise'; 
+import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Smit@2025',
-  database: 'golfy_travel',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-      multipleStatements: true   
+  multipleStatements: true,
+  ssl: { rejectUnauthorized: false }
 });
-
 
 export default pool;
